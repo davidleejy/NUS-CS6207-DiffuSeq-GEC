a8434b6ef67f5bbdc574d8e7e64434e437e7cbae
david <david>
2023-04-03 10:25:42 +0800

    change bsz of run_decode scripts

---
+++ scripts/run_decode_beadev_errtocorr.sh
@@ -0,0 +1,8 @@
+python -u run_decode.py \
+--model_dir diffusion_models/diffuseq_gec_h128_lr0.0001_t2000_sqrt_lossaware_seed102_gec_err2corr120230327-14:42:44 \
+--model_chkpt ema_0.9999_040000 \
+--seed 124 \
+--split test-bea2019-dev \
+--bsz 35 \
+--step 2000
+
